de16ba415d9211cd543896c804ff6f43dfb4d59e
Abi Aryaza <abiaryaza@gmail.com>
2021-11-30 21:35:20 +0700

    Add Edit Akun Layout

--- app/src/main/java/com/example/smartcityta/view/home/account/EditAccountActivity.kt
+++ app/src/main/java/com/example/smartcityta/view/home/account/EditAccountActivity.kt
@@ -1,12 +1,42 @@
 package com.example.smartcityta.view.home.account
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.smartcityta.R
+import com.example.smartcityta.databinding.ActivityEditAccountBinding
+import com.example.smartcityta.view.home.HomeActivity
 
 class EditAccountActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditAccountBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_account)
+        binding = ActivityEditAccountBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.buttonExitEdit.setOnClickListener{
+            val moveIntent = Intent(this, HomeActivity::class.java)
+            startActivity(moveIntent)
+        }
+        binding.buttonEditAccount.setOnClickListener{
+            val username = binding.textEditAccountUsername.text.toString()
+            val email = binding.textEditAccountEmail.text.toString()
+            val nama = binding.textEditAccountNama.text.toString()
+            val alamat = binding.textEditAccountAlamat.text.toString()
+            Log.d("CheckEdit", "${username} ${email} ${nama} ${alamat}")
+        }
+
     }
+
+
+
+
+
+
+
+
 }
