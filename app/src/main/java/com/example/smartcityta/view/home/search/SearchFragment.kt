adb3452305a530748b822f1d1dacfa5104d9d705
Arief Daffa <ariefkeren76@yahoo.com>
2021-12-01 08:09:06 +0700

    add feature search

--- app/src/main/java/com/example/smartcityta/view/home/search/SearchFragment.kt
+++ app/src/main/java/com/example/smartcityta/view/home/search/SearchFragment.kt
@@ -1,19 +1,34 @@
 package com.example.smartcityta.view.home.search
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.smartcityta.R
 import com.example.smartcityta.databinding.FragmentSearchBinding
+import com.example.smartcityta.datasource.network.models.PerpustakaanResponseItem
+import com.example.smartcityta.view.home.detailperpustakaan.DetailPerpustakaanActivity
+import com.example.smartcityta.view.home.home.HomeViewModel
+import com.example.smartcityta.view.home.home.ListPerpustakaanAdapter
+import com.example.smartcityta.view.home.home.SearchPerpustakaanAdapter
+import com.example.smartcityta.view.home.home.SearchViewModel
 
 class SearchFragment : Fragment() {
 
     private lateinit var searchViewModel: SearchViewModel
     private var _binding: FragmentSearchBinding? = null
+    private val viewModel: HomeViewModel by viewModels()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -30,11 +45,43 @@ class SearchFragment : Fragment() {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        searchViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val rvPerpustakaan: RecyclerView = view.findViewById(R.id.rv_search_perpustakaan)
+
+        binding.etSearchPerpustakaan.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                Log.d("Logg","onTextChanged: ${s}")
+
+            }
+
+
+        })
+
+        binding.btnSearch.setOnClickListener{
+
+        }
+
+        viewModel.perpustakaans.observe(viewLifecycleOwner){perpustakaans->
+            val listPerpustakaanAdapter = SearchPerpustakaanAdapter(perpustakaans)
+            rvPerpustakaan.adapter = listPerpustakaanAdapter
+            rvPerpustakaan.layoutManager = LinearLayoutManager(requireContext())
+        }
+
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/example/smartcityta/view/home/search/SearchPerpustakaanAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.smartcityta.view.home.home
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.smartcityta.BuildConfig.BASE_URL_CMS
+import com.example.smartcityta.R
+import com.example.smartcityta.datasource.network.models.PerpustakaanResponseItem
+
+class SearchPerpustakaanAdapter(private val listPerpustakaan:List<PerpustakaanResponseItem>)
+    :RecyclerView.Adapter<SearchPerpustakaanAdapter.ListViewHolder>() {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ListViewHolder {
+        val view:View = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_row_perpustakaan, viewGroup, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val perpus = listPerpustakaan[position]
+        holder.textNamaPerpustakaan.text = perpus.nama
+        holder.textAlamatPerpustakaan.text = perpus.alamat
+        holder.textJumlahPerpustakaan.text = "${perpus.totalPengunjung} Pengunjung"
+        Glide.with(holder.itemView.context).load("${BASE_URL_CMS}${perpus.gambar[0].url}").into(holder.imageViewPerpustakaan)
+
+    }
+
+    override fun getItemCount(): Int {
+        return listPerpustakaan.size
+    }
+
+    class ListViewHolder(itemView:View): RecyclerView.ViewHolder(itemView) {
+        var textNamaPerpustakaan:TextView = itemView.findViewById(R.id.textNamaPerpustakaan)
+        var textAlamatPerpustakaan:TextView = itemView.findViewById(R.id.textAlamatPerpustakaan)
+        var textJumlahPerpustakaan:TextView = itemView.findViewById(R.id.textPengunjungPerpustakaan)
+        var imageViewPerpustakaan:ImageView = itemView.findViewById(R.id.imageViewPerpustakaan)
+        var buttonLihatDetail:Button = itemView.findViewById(R.id.buttonLihatDetail)
+    }
+
+    interface OnItemClickListener{
+        fun onItemClicked(perpustakaan:PerpustakaanResponseItem)
+    }
+
+}
